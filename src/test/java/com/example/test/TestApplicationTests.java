1e7df42bbbb8dda831d1c31b2faef09f3c08d76f
Lihong <lhl1987318@163.com>
2019-01-08 18:37:14 +0800

    测试es

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -53,14 +53,14 @@ public class TestApplicationTests {
         //amqpTemplate.convertAndSend("amq.direct",);
 
 
-        //创建exchange
-        //amqpAdmin.declareExchange(new DirectExchange("my-dir"));
-
-        //创建 queues
-        //amqpAdmin.declareQueue(new Queue("myemp"));
-
-        //创建绑定关系
-        //amqpAdmin.declareBinding(new Binding("myemp" , Binding.DestinationType.QUEUE,"my-dir","my-rout",null));
+//        //创建exchange
+//        amqpAdmin.declareExchange(new DirectExchange("my-dir"));
+//
+//        //创建 queues
+//        amqpAdmin.declareQueue(new Queue("myemp"));
+//
+//        //创建绑定关系
+//        amqpAdmin.declareBinding(new Binding("myemp" , Binding.DestinationType.QUEUE,"my-dir","my-rout",null));
 
         amqpTemplate.convertAndSend("myemp",new Employee("zhansn",12));
     }
